84125c92bea5e231d18c4dcddb83b45dca49f40c
Noah Tomkins <1tomkinsnoa@gmail.com>
2022-10-14 02:14:50 -0400

    Started working on Form, also cleaned up resource file locations

--- app/src/main/java/ca/tnoah/frc/scouting/models/template/TemplateSerializer.java
+++ app/src/main/java/ca/tnoah/frc/scouting/models/template/TemplateSerializer.java
@@ -179,4 +179,40 @@ public class TemplateSerializer {
         }
     }
 
+    public static TemplateData getSample() {
+        return TemplateSerializer.deserialize("<Scout name=\"Test Template\" type=\"Pit\" year=\"2020\" >\n" +
+                "    <Section>\n" +
+                "        <Header>Scout Info</Header>\n" +
+                "        <Fields>\n" +
+                "            <TextField label=\"Scout Name\" />\n" +
+                "            <TextField label=\"Match Number\" />\n" +
+                "        </Fields>\n" +
+                "    </Section>\n" +
+                "    <Section>\n" +
+                "        <Header>Drivetrain</Header>\n" +
+                "        <Fields>\n" +
+                "            <RadioList label=\"Drivetrain Type\" other=\"true\">\n" +
+                "                <Item label=\"4 Wheel Tank\" />\n" +
+                "                <Item label=\"6 Wheel Tank\" />\n" +
+                "                <Item label=\"Mecanum\" />\n" +
+                "                <Item label=\"Swerve\" />\n" +
+                "            </RadioList>\n" +
+                "        </Fields>\n" +
+                "    </Section>\n" +
+                "    <Section>\n" +
+                "        <Header>Auto</Header>\n" +
+                "        <Fields>\n" +
+                "            <CheckList label=\"Ports during auto\">\n" +
+                "                <Item label=\"Lower\" />\n" +
+                "                <Item label=\"High\" />\n" +
+                "                <Item label=\"Inner\" />\n" +
+                "            </CheckList>\n" +
+                "            <Counter label=\"How many fuel cells does your robot start with?\" max=\"5\" />\n" +
+                "            <Checkbox label=\"Can it cross the baseline?\" />\n" +
+                "            <TextField label=\"Robot Weight\" />\n" +
+                "        </Fields>\n" +
+                "    </Section>\n" +
+                "</Scout>");
+    }
+
 }
---
+++ app/src/main/java/ca/tnoah/frc/scouting/ui/form/FormActivity.java
@@ -0,0 +1,102 @@
+package ca.tnoah.frc.scouting.ui.form;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import ca.tnoah.frc.scouting.R;
+import ca.tnoah.frc.scouting.models.template.TemplateData;
+import ca.tnoah.frc.scouting.models.template.TemplateSerializer;
+
+public class FormActivity extends AppCompatActivity {
+
+    LinearLayout linearLayout;
+    LayoutInflater inflater;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form);
+
+        inflater = LayoutInflater.from(this);
+        linearLayout = findViewById(R.id.form_linear_layout);
+
+        TemplateData templateData = TemplateSerializer.getSample();
+        createForm(templateData);
+    }
+
+    private void createForm(TemplateData templateData) {
+        for (TemplateData.Section section : templateData.sections)
+            createSection(section);
+    }
+
+    @SuppressLint("MissingInflatedId")
+    private void createSection(TemplateData.Section section) {
+        View child = inflater.inflate(R.layout.form_section, null);
+        linearLayout.addView(child);
+
+        ((TextView) child.findViewById(R.id.form_section_header)).setText(section.header);
+
+        for (TemplateData.Field field : section.fields)
+            createField(child.findViewById(R.id.form_section_linear_Layout), field);
+    }
+
+    private void createField(LinearLayout layout, TemplateData.Field field) {
+        if (field instanceof TemplateData.TextField)
+            createTextField(layout, (TemplateData.TextField) field);
+        if (field instanceof TemplateData.RadioList)
+            createRadioGroup(layout, (TemplateData.RadioList) field);
+        if (field instanceof TemplateData.CheckList)
+            createCheckList(layout, (TemplateData.CheckList) field);
+    }
+
+    private void createTextField(LinearLayout parent, TemplateData.TextField textField) {
+        View child = inflater.inflate(R.layout.form_text_field, null);
+        parent.addView(child);
+
+        ((TextView) child.findViewById(R.id.formTextFieldLabel)).setText(textField.label);
+        ((TextView) child.findViewById(R.id.formTextFieldValue)).setText(textField.value);
+    }
+
+    private void createRadioGroup(LinearLayout parent, TemplateData.RadioList radioList) {
+        View child = inflater.inflate(R.layout.form_radio_list, null);
+        parent.addView(child);
+
+        ((TextView) child.findViewById(R.id.formRadioListLabel)).setText(radioList.label);
+        RadioGroup radioGroup = child.findViewById(R.id.formRadioListRadioGroup);
+
+        for (TemplateData.Item item : radioList.items) {
+            RadioButton radioButton = new RadioButton(this);
+            radioButton.setText(item.label);
+            radioGroup.addView(radioButton);
+        }
+
+        if (radioList.other) {
+            RadioButton radioButton = new RadioButton(this);
+            radioButton.setText(R.string.other);
+            radioGroup.addView(radioButton);
+        }
+    }
+
+    private void createCheckList(LinearLayout parent, TemplateData.CheckList checkList) {
+        View child = inflater.inflate(R.layout.form_check_list, null);
+        parent.addView(child);
+
+        ((TextView) child.findViewById(R.id.formCheckListLabel)).setText(checkList.label);
+        LinearLayout checkGroup = findViewById(R.id.formCheckListCheckGroup);
+
+        for (TemplateData.Item item : checkList.items) {
+            CheckBox checkBox = new CheckBox(this);
+            checkBox.setText(item.label);
+            checkGroup.addView(checkBox);
+        }
+    }
+}
